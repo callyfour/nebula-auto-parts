27375197e372d6e6f80ca3a8b564bb2f444392e6
callyfour <ktsurugi420@gmail.com>
2025-09-26 05:52:49 +0800

    asd

--- Documents/PORCALFILESCODE/SIA/nebula-auto-parts/server/server.js
+++ Documents/PORCALFILESCODE/SIA/nebula-auto-parts/server/server.js
@@ -18,7 +18,7 @@ const JWT_SECRET = process.env.JWT_SECRET || "fallback_secret";
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  "https://your-app-name.onrender.com/api/auth/google/callback"
+  process.env.GOOGLE_REDIRECT_URI
 );
 
 
